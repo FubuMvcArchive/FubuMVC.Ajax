ed909f76a3574809b45cdd5eefe766a811d6a85a
Josh Arnold <rncodepoet@gmail.com>
2012-06-30 15:03:35 -0500

    Extension/rake/nuget fixes

--- src/FubuMVC.Ajax/RequestCorrelation.cs
+++ src/FubuMVC.Ajax/RequestCorrelation.cs
@@ -1,15 +1,17 @@
 ﻿using System.Collections.Generic;
+using FubuMVC.Core;
 using FubuMVC.Core.Registration;
 
 namespace FubuMVC.Ajax
 {
-	public class RequestCorrelation : IConfigurationAction
+	public class RequestCorrelation : IFubuRegistryExtension
 	{
-		public void Configure(BehaviorGraph graph)
+		public void Configure(FubuRegistry registry)
 		{
-			graph
+			registry.Services(x => x.SetServiceIfNone<ICorrelateRequests, CorrelateRequests>());
+			registry.Configure(graph => graph
 				.Behaviors
-				.Each(chain => chain.Prepend(new SetCorrelationHeadersNode()));
+				.Each(chain => chain.Prepend(new SetCorrelationHeadersNode())));
 		}
 	}
 }
