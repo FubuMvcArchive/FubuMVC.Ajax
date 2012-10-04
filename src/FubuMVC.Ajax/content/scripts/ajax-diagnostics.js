86b8e49321562339f306cd8482b173b736827f33
jmarnold <rncodepoet@gmail.com>
2012-10-04 14:53:09 -0500

    Adds the reusable js pieces

---
+++ src/FubuMVC.Ajax/content/scripts/ajax-diagnostics.js
@@ -0,0 +1,26 @@
+﻿(function () {
+    var pendingRequests = [];
+    var updateRequests = function () {
+        $('#PendingRequests').val(pendingRequests.length);
+    };
+
+    $.continuations.bind('AjaxStarted', function (request) {
+        pendingRequests.push(request);
+        updateRequests();
+    });
+
+    $.continuations.bind('AjaxCompleted', function (response) {
+        var id = response.correlationId;
+        var tmpRequests = [];
+        for (var i = 0; i < pendingRequests.length; i++) {
+            var r = pendingRequests[i];
+            if (r.correlationId != id) {
+                tmpRequests.push(r);
+            }
+        }
+
+        pendingRequests.length = 0;
+        pendingRequests = tmpRequests;
+        updateRequests();
+    });
+} ());
