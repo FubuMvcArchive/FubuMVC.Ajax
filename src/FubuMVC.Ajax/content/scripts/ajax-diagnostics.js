f16035f438c0ddd9740dad38cc4aa0cbc3f3677b
jmarnold <rncodepoet@gmail.com>
2013-07-10 22:14:24 -0500

    Bumped up to the latest fubucontinuations.js

--- src/FubuMVC.Ajax/content/scripts/ajax-diagnostics.js
+++ src/FubuMVC.Ajax/content/scripts/ajax-diagnostics.js
@@ -1,34 +1,36 @@
 ﻿(function ($, continuations) {
-    var pendingRequests = [];
-    var initialized = false;
-    
-    var updateRequests = function () {
-        var id = 'PendingRequests';
-        if(!initialized) {
-            $('<input type="hidden" value="0" />').attr("id", id).appendTo('body');
-            initialized = true;
-        }
-        
-        $('#' + id).val(pendingRequests.length);
-    };
+  var pendingRequests = [];
+  var elementId = 'PendingRequests';
 
-    continuations.bind('AjaxStarted', function (request) {
-        pendingRequests.push(request);
-        updateRequests();
-    });
+  var updateRequests = function () {
+    $('#' + id).val(pendingRequests.length);
+  };
 
-    continuations.bind('AjaxCompleted', function (response) {
-        var id = response.correlationId;
-        var tmpRequests = [];
-        for (var i = 0; i < pendingRequests.length; i++) {
-            var r = pendingRequests[i];
-            if (r.correlationId != id) {
-                tmpRequests.push(r);
-            }
-        }
+  continuations.bind('AjaxStarted', function (request) {
+    pendingRequests.push(request);
+    updateRequests();
+  });
 
-        pendingRequests.length = 0;
-        pendingRequests = tmpRequests;
-        updateRequests();
-    });
-} (jQuery, jQuery.continuations));
+  continuations.bind('AjaxCompleted', function (response) {
+    var id = response.correlationId;
+    var tmpRequests = [];
+    for (var i = 0; i < pendingRequests.length; i++) {
+      var r = pendingRequests[i];
+      if (r.correlationId != id) {
+        tmpRequests.push(r);
+      }
+    }
+
+    pendingRequests.length = 0;
+    pendingRequests = tmpRequests;
+    updateRequests();
+  });
+
+  $(function () {
+    var element = $('#' + elementId);
+    if (element.size() == 0) {
+      $('<input type="hidden" value="0" />').attr("id", elementId).appendTo('body');
+    }
+  });
+
+}(jQuery, jQuery.continuations));
